c34213ba3909323b27132e1fcd50df5db29e640e
Arafat <mdarafat3167@gmail.com>
2025-12-20 11:48:01 +0600

    allBooks file implement with book card

--- src/components/BookCard/BookCard.jsx
+++ src/components/BookCard/BookCard.jsx
@@ -1,27 +1,25 @@
-const BookCard = () => {
-  //   const { title, author, price, image } = book;
+const BookCard = ({ book }) => {
+  const { title, author, price, image } = book;
 
   return (
     <div className="card bg-base-100 border border-base-300 shadow-sm hover:shadow-md transition">
       {/* Book Image */}
       <figure className="h-56 bg-base-200 overflow-hidden">
-        <img src="" alt="" className="h-full w-full object-cover" />
+        <img src={image} alt={title} className="w-full h-full object-cover" />
       </figure>
 
-      <div className="card-body">
+      <div className="card-body text-center">
         {/* Title */}
-        <h2 className="card-title text-neutral text-lg font-semibold">
-          Rich Dad Poor Dad
-        </h2>
+        <h2 className="text-center text-lg font-semibold">{title}</h2>
 
         {/* Author */}
         <p className="text-sm text-neutral/70">
-          by <span className="font-medium">Robert T. Kiyosaki</span>
+          by <span className="font-medium">{author}</span>
         </p>
 
         {/* Price */}
         <div className="mt-3">
-          <span className="text-primary text-lg font-bold">$ 200</span>
+          <span className="text-primary text-lg font-bold">$ {price}</span>
         </div>
       </div>
     </div>
